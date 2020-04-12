560948237944caf773cb52d26cef77324bf8dd64
Jared Taylor <jared.xyz@mailbox.org>
2020-04-12 19:50:06 -0400

    Can now insert rows. Script checks if the data type is wrong. A database.csv file is created if it does not exist when the script is run.

--- database.rb
+++ database.rb
@@ -1,7 +1,7 @@
 require 'terminal-table'
 require 'json'
 
-#-- Read file containing Fields object and obtain properties --#
+#-- Read schema file containing Fields object and obtain properties --#
 parsed = JSON.parse(File.read("schema.json"));
 properties = parsed["fields"];
 
@@ -13,20 +13,87 @@ properties.each do |property|
     schema[property["name"]] = property["type"]
 end
 
-#-- Create the terminal table --#
-rows = []
-rows << schema.keys
-table = Terminal::Table.new :rows => rows
+#-- Read database file by line into array --#
+data = []
+if not File.exist?("database.csv")
+    database = File.new("database.csv", "w")
+    database.close
+end
+database = File.open("database.csv", "r")
+database.each_line do |line|
+    row = line.split(",")
+    data << row
+end
+database.close
 
-def print_examples(schema)
-    example = create_insert_example(schema.values)
-    puts example
+def create_terminal_table(headings, data)
+    return Terminal::Table.new :title => "Database", :headings => headings do |t|
+        data.each do |row|
+            t.add_row row
+        end
+        t.style = {:all_separators => true}
+    end
 end
 
-def handle_input(query, schema)
+def print_examples(schema)
+    insert_example = create_insert_example(schema.values)
+    delete_example = "Example deletion:  delete where (age < 18)"
+    filter_example = "Example selection: select where (honor student = true)"
+    puts insert_example
+    puts delete_example
+    puts filter_example
+end
+
+def is_integer(num)
+    Integer(num) rescue return false
+    return true
+end
+
+def is_float(num)
+    Float(num) rescue return false
+    return true
+end
+
+def is_boolean(bool)
+    bool == "true" || bool == "false"
+end
+
+def insert_data(query, schema, data)
+    new_row = query.split("(").last.split(")").first.delete(' ').downcase
+    split_row = new_row.split(",")
+    split_row.zip(schema.values).each do |value, type|
+        case type
+        when "integer"
+            if not is_integer(value)
+                puts "#{value} is not an integer."
+                return data
+            end
+        when "float"
+            if not is_float(value)
+                puts "#{value} is not a float."
+                return data
+            end
+        when "boolean"
+            if not is_boolean(value)
+                puts "#{value} is not a boolean."
+                return data
+            end
+        end
+    end
+
+    #-- The data is valid. Append it to the file and return the updated data set --#
+    open("database.csv", "a") { |file|
+        file.puts new_row + "\n"
+    }
+    data << split_row
+    puts "Row added succesfully."
+    return data
+end
+
+def handle_input(query, schema, data)
     query = query.downcase
     if query.include? "insert"
-        puts "Attemping insert"
+        data = insert_data(query, schema, data)
     elsif query.include? "delete"
         puts "Deleting"
     elsif query.include? "select"
@@ -38,6 +105,7 @@ def handle_input(query, schema)
     else
         puts "Command not received. Please see the example"
     end
+    return data
 end
 
 def create_insert_example(types)
@@ -79,11 +147,13 @@ def pause()
 end
 
 query = nil
+table = create_terminal_table(schema.keys, data)
 
 while query != 'q'
     clear_screen()
     default_display(table)
     query = gets.chomp
-    handle_input(query, schema)
+    data = handle_input(query, schema, data)
+    table = create_terminal_table(schema.keys, data)
     pause()
 end
